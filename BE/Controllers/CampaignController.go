442816af5360678afa1bc6352075c9500c4f62aa
Ly <huynhcamly2005@gmail.com>
2026-05-19 12:29:10 +0700

    "feat: implement paginated KOL listing"

--- BE/Controllers/CampaignController.go
+++ BE/Controllers/CampaignController.go
@@ -5,6 +5,7 @@ import (
 	"wan-api-kol-event/Const"
 	"wan-api-kol-event/Logic"
 	"wan-api-kol-event/ViewModels"
+	"wan-api-kol-event/Utils"
 
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
@@ -14,31 +15,52 @@ func GetKolsController(context *gin.Context) {
 	var KolsVM ViewModels.KolViewModel
 	var guid = uuid.New().String()
 
-	// * Get Kols from the database based on the range of pageIndex and pageSize
-	// * TODO: Implement the logic to get parameters from the request
-	// ? If parameter passed in the request is not valid, return the response with HTTP Status Bad Request (400)
-	// @params: pageIndex
-	// @params: pageSize
+	// extract pageIndex and pageSize from query params
+	pageIndexStr := context.DefaultQuery("pageIndex", "1")
+	pageSizeStr := context.DefaultQuery("pageSize", "100")
 
-	// * Perform Logic Here
-	// ! Pass the parameters to the Logic Layer
+	// Convert to integers using helpers
+	pageIndex := Utils.StringToInt64(pageIndexStr)
+	pageSize := Utils.StringToInt64(pageSizeStr)
+
+	// Validate
+    if pageIndex < 1 {
+        KolsVM.Result = Const.UnSuccess
+        KolsVM.ErrorMessage = "Invalid pageIndex"
+        KolsVM.PageIndex = 1
+        KolsVM.PageSize = 100
+        KolsVM.Guid = guid
+        context.JSON(http.StatusBadRequest, KolsVM)
+        return
+    }
+
+    if pageSize < 1 {
+        KolsVM.Result = Const.UnSuccess
+        KolsVM.ErrorMessage = "Invalid pageSize"
+        KolsVM.PageIndex = pageIndex
+        KolsVM.PageSize = 100
+        KolsVM.Guid = guid
+        context.JSON(http.StatusBadRequest, KolsVM)
+        return
+    }
+	 
+	// Call Logic Layer
 	kols, error := Logic.GetKolLogic()
 	if error != nil {
 		KolsVM.Result = Const.UnSuccess
 		KolsVM.ErrorMessage = error.Error()
-		KolsVM.PageIndex = 1 // * change this to the actual page index from the request
-		KolsVM.PageSize = 10 // * change this to the actual page size from the request
+		KolsVM.PageIndex = pageIndex  
+		KolsVM.PageSize = pageSize  
 		KolsVM.Guid = guid
 		context.JSON(http.StatusInternalServerError, KolsVM)
 		return
 	}
 
-	// * Return the response after the logic is executed
-	// ? If the logic is successful, return the response with HTTP Status OK (200)
+	// Return successful response 
 	KolsVM.Result = Const.Success
 	KolsVM.ErrorMessage = ""
-	KolsVM.PageIndex = 1 // * change this to the actual page index from the request
-	KolsVM.PageSize = 10 // * change this to the actual page size from the request
+	KolsVM.PageIndex = pageIndex 
+	KolsVM.PageSize = pageSize  
 	KolsVM.Guid = guid
 	KolsVM.KOL = kols
 	KolsVM.TotalCount = int64(len(kols))
--- BE/Logic/KOLLogic.go
+++ BE/Logic/KOLLogic.go
@@ -1,6 +1,10 @@
 package Logic
 
-import "wan-api-kol-event/DTO"
+import (
+	"wan-api-kol-event/DTO"
+	"wan-api-kol-event/Initializers"
+    "wan-api-kol-event/Models"
+)
 
 // * Get Kols from the database based on the range of pageIndex and pageSize
 // ! USE GORM TO QUERY THE DATABASE
@@ -11,5 +15,47 @@ import "wan-api-kol-event/DTO"
 // @return: List of KOLs and error message
 func GetKolLogic() ([]*DTO.KolDTO, error) {
 
-	return nil, nil
+	var kols []Models.Kol
+	
+	// query all Kols from database
+	if err := Initializers.DB.Find(&kols).Error; err != nil {
+		return nil, err
+	}
+
+	// Convert Models.Kol to DTO.KolDTO
+    var kolDTOs []*DTO.KolDTO
+    for _, kol := range kols {
+        dto := &DTO.KolDTO{
+            KolID:                kol.KolID,
+            UserProfileID:        kol.UserProfileID,
+            Language:             kol.Language,
+            Education:           kol.Education,
+            ExpectedSalary:       kol.ExpectedSalary,
+            ExpectedSalaryEnable: kol.ExpectedSalaryEnable,
+            ChannelSettingTypeID: kol.ChannelSettingTypeID,
+            IDFrontURL:           kol.IDFrontURL,
+            IDBackURL:            kol.IDBackURL,
+            PortraitURL:          kol.PortraitURL,
+            RewardID:             kol.RewardID,
+            PaymentMethodID:      kol.PaymentMethodID,
+            TestimonialsID:       kol.TestimonialsID,
+            VerificationStatus:   kol.VerificationStatus,
+            Enabled:              kol.Enabled,
+            ActiveDate:           kol.ActiveDate,
+            Active:               kol.Active,
+            CreatedBy:            kol.CreatedBy,
+            CreatedDate:          kol.CreatedDate,
+            ModifiedBy:           kol.ModifiedBy,
+            ModifiedDate:         kol.ModifiedDate,
+            IsRemove:             kol.IsRemove,
+            IsOnBoarding:         kol.IsOnBoarding,
+            Code:                 kol.Code,
+            PortraitRightURL:     kol.PortraitRightURL,
+            PortraitLeftURL:      kol.PortraitLeftURL,
+            LivenessStatus:       kol.LivenessStatus,
+        }
+        kolDTOs = append(kolDTOs, dto)
+    }
+
+	return kolDTOs, nil
 }
