a2cc1c9e60e3c6af11f85295f88f5ee70a41712e
Ly <huynhcamly2005@gmail.com>
2026-05-20 09:54:12 +0700

    "fix(cors): allow localhost:3000 origin"

--- BE/main.go
+++ BE/main.go
@@ -6,6 +6,7 @@ import (
 	"wan-api-kol-event/Initializers"
 
 	"github.com/gin-gonic/gin"
+	"github.com/gin-contrib/cors"
 )
 
 func init() {
@@ -15,6 +16,12 @@ func init() {
 
 func main() {
 	r := gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins: []string{"http://localhost:3000"},
+		AllowMethods: []string{"GET", "POST", "PUT", "DELETE"},
+        AllowHeaders: []string{"Content-Type", "Authorization"},
+	}))
 
 	// Define your Gin routes here
 	r.GET("/kols", Controllers.GetKolsController)
